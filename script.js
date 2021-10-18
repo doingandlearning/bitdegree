2c665770a28eb47d8e098d94bcec1503ecfce56f
Kevin Cunningham <kevin@kevincunningham.co.uk>
2021-10-18 17:11:15 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const colorArray = [
+  "red",
+  "yellow",
+  "green",
+  "brown",
+  "orange",
+  "purple",
+  "gold",
+];
+// splice
+
+// slice
+// console.log(colorArray, colorArray.slice(-1));
+
+// concat
+// console.log(colorArray.concat(["black", "white", "pink"], 3, true));
+
+// indexOf
+// console.log(colorArray.indexOf("red", 0));
+
+// lastIndexOf
+// console.log(colorArray.lastIndexOf("red", 10));
+
+// includes
+// console.log(colorArray.includes("reddish"));
+
+// reverse
+// console.log(colorArray.reverse());
+
+// join
+// console.log(`My favourite colours are ${colorArray.join(" & ")}.`);
+
+// split
+const split = "How I love to eat pie".split(" ");
+console.log(split.reverse());
